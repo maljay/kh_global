0ef8291f7378309ec6fa0dac28510100db589295
Jamal Aminu <mj.aminu73@gmail.com>
2023-11-26 09:31:51 +0100

    refactored index.js to solve cors problem_

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 //import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 //import compress from 'compression'
-import cors from 'cors'
+//import cors from 'cors'
 import "dotenv/config.js"; // allows us to use environment variables
 //import helmet from 'helmet'
 import connectDB from './connectDB/connect.js'
@@ -11,6 +11,27 @@ import authRoutes from './routes/auth.routes.js'
 
 const app = express()
 
+// Set middleware of CORS 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 //app.use(bodyParser.json())
 //app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
@@ -23,7 +44,7 @@ Solve a bug (..The value of the 'Access-Control-Allow-Origin'
 header in the response must not be the wildcard '*' when the
 request's credentials mode is 'include'... ) with following
 lines (24 - 29)
-*/
+
 var corsOptions = {
   origin: ["https://kh-global-links.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -32,8 +53,8 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
+*/
 
-//app.use(cors());
 /**
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','https://kh-global.onrender.com');
