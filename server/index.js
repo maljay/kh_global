a2b78140ecf20820d3cdf94e7f0c3411003076df
Jamal Aminu <mj.aminu73@gmail.com>
2023-11-27 08:39:11 +0100

    added a middleware to index.js - cookies

--- server/index.js
+++ server/index.js
@@ -15,6 +15,14 @@ app.use(
   })
 );
 
+//Temp for debugging
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 app.use(cookieParser());
 
 app.use(express.json());
